2488dce60abdb68d7c458e721d2b7701dc0ceb9c
LimonSeo <53857835+ilimon@users.noreply.github.com>
2021-12-14 04:50:10 +0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -193,3 +193,6 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
+
+/*==================== Start Animation ====================*/
+AOS.init();
